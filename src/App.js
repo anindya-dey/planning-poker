3fa50e0996c872d46357607523cdfd4178f01570
Anindya Dey <anindya.dey@vista.com>
2022-10-12 14:02:59 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 
 import Login from "./components/Login";
+import NewRoom from "./components/NewRoom";
+import Room from "./components/Room";
 
 function App() {
-  const [ roomSessionId, setRoomSessionId ] = useState();
+  const [roomSessionId, setRoomSessionId] = useState();
 
   return (
     <>
-      <div>{ roomSessionId }</div>
-      <Login onCreateRoom={() => setRoomSessionId(uuid())} onJoinRoom={() => console.log("Joined!!!!")}/>
+      <Routes>
+        <Route path="/" element={<Login onCreateRoom={() => setRoomSessionId(uuid())} onJoinRoom={() => console.log("Joined!!!!")} />} />
+        <Route path="/room" element={<NewRoom />} />
+        <Route path="/room/:roomSessionId" element={<Room roomSessionId={roomSessionId} />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/NewRoom.js
@@ -0,0 +1,9 @@
+export default function NewRoom() {
+    return (
+        <>
+        <div>
+            New Room
+        </div>
+        </>
+    )
+}
