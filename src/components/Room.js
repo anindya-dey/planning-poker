10b11e4dc667299b65eda3bc9fbae95a4cd55626
Anindya Dey <anindya.dey@vista.com>
2022-10-12 16:52:24 +0530

    add Room view

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,30 @@
 import { useParams } from "react-router-dom"
+import GoToHome from "./GoToHome";
+import TileLayout from "./TileLayout";
 
 export default function Room() {
     const params = useParams();
 
     console.log(params);
 
-    return <div>{ params.roomId }</div>
+    return (
+        <TileLayout>
+            <div className="text-center">
+                <div className="text-3xl text-indigo-600">RoomName | Planning Poker</div>
+                <div className="mt-4 text-xl text-indigo-400 font-thin">(Join this room using RoomId)</div>
+                <div className="mt-10 grid grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className="text-sm text-indigo-600 px-2 py-10 border border-indigo-600 rounded-lg">Anindya</div>
+                    <div className="text-sm text-indigo-600 px-2 py-10 border border-indigo-600 rounded-lg">Apoorv</div>
+                    <div className="text-sm text-indigo-600 px-2 py-10 border border-indigo-600 rounded-lg">Chris</div>
+                    <div className="text-sm text-indigo-600 px-2 py-10 border border-indigo-600 rounded-lg">Dhruv</div>
+                    <div className="text-sm text-indigo-600 px-2 py-10 border border-indigo-600 rounded-lg">Niket</div>
+                    <div className="text-sm text-indigo-600 px-2 py-10 border border-indigo-600 rounded-lg">Rishit</div>
+                </div>
+
+                <div className="mt-6 text-center">
+                    <GoToHome />
+                </div>
+            </div>
+        </TileLayout>
+    )
 }
--- src/components/TileLayout.js
+++ src/components/TileLayout.js
@@ -1,7 +1,7 @@
 export default function TileLayout({ children }) {
     return (
         <div className="flex justify-center md:items-center h-screen selection:bg-indigo-600 selection:text-white">
-            <div className="md:w-6/12 lg:w-4/12 xl:w-3/12 px-6 py-16 md:border md:border-gray-200 md:rounded-lg md:shadow-lg md:shadow-indigo-100">
+            <div className="px-10 py-10 md:border md:border-gray-200 md:rounded-lg md:shadow-lg md:shadow-indigo-100">
                 {children}
             </div>
         </div>
