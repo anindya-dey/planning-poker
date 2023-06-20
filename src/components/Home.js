87fa809e5e2b3b09312cc108024054e0ddad5422
Anindya Dey <andy.ani1992@gmail.com>
2023-06-20 22:15:16 +0530

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 import Divider from "./Divider";
 
 export default function Home() {
-    const [roomId, setRoomId] = useState('')
+    const [roomId, setRoomId] = useState("");
+    const [username, setUserName] = useState("");
+    
     return (
-        <div className="w-full flex flex-col space-y-10 justify-center">
-            <div className="md:mb-4 text-3xl text-center text-indigo-600">Planning Poker</div>
+        <div className="flex flex-col justify-center w-full space-y-10">
+            <div className="text-3xl text-center text-indigo-600 md:mb-4">Planning Poker</div>
             <div>
                 <form className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-2">
-                    <input type="text" className="py-2 md:w-3/5 rounded border border-gray-300 placeholder:text-indigo-300 text-indigo-600 focus:ring-indigo-600" placeholder="Room Id" value={roomId} onChange={(e) => setRoomId(e.target.value)} />
+                    <input type="text" className="py-2 text-indigo-600 border border-gray-300 rounded md:w-3/5 placeholder:text-indigo-300 focus:ring-indigo-600" placeholder="Room Id" value={roomId} onChange={(e) => setRoomId(e.target.value)} />
+                    <input type="text" className="py-2 text-indigo-600 border border-gray-300 rounded md:w-3/5 placeholder:text-indigo-300 focus:ring-indigo-600" placeholder="Your Name" value={username} onChange={(e) => setUserName(e.target.value)} />
                     <Link className="md:w-2/5" to={`/room/${roomId}`}>
-                        <button type="submit" className="px-6 py-2 w-full bg-indigo-600 text-white text-center rounded hover:bg-indigo-800 hover:cursor-pointer transition-all ease-in-out">Join</button>
+                        <button type="submit" className="w-full px-6 py-2 text-center text-white transition-all ease-in-out bg-indigo-600 rounded hover:bg-indigo-800 hover:cursor-pointer">Join</button>
                     </Link>
                 </form>
             </div>
@@ -20,7 +23,7 @@ export default function Home() {
 
             <div>
                 <Link to={"/room/new"}>
-                    <div className="px-6 py-2 rounded-md text-center text-indigo-600 border border-indigo-600 hover:text-white hover:bg-indigo-600 hover:cursor-pointer transition-all ease-in-out">Create new room</div>
+                    <div className="px-6 py-2 text-center text-indigo-600 transition-all ease-in-out border border-indigo-600 rounded-md hover:text-white hover:bg-indigo-600 hover:cursor-pointer">Create new room</div>
                 </Link>
             </div>
         </div>
